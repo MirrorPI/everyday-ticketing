aebaba5246515207f45bcb0e3e8f6ff42ffa935f
MirrorPI <sungsoo6414@naver.com>
2026-07-09 13:45:48 +0900

    chore: Spring Boot 프로젝트 생성

---
+++ src/main/java/com/everyday/ticketing/EverydayTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.everyday.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydayTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverydayTicketingApplication.class, args);
+	}
+
+}
